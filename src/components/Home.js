7a3d166407ba995a09e51c2dfd2328adbb240a10
Reshmi <rimjhim11998@gmail.com>
2021-11-02 02:10:17 +0530

    reshmi's first commit

---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class Home extends React.Component{
+    render(){
+        return(
+            <h1>This is our home page it is bad.we will be back soon</h1>
+        );
+    }
+}
+
+export default Home;
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import {BrowserRouter as Router,Switch ,Route,Link } from "react-router-dom";
+  
+ReactDOM.render(
+    <App />,
+  document.getElementById('root')
+);
+
