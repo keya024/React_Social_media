602cbc2d8adb0f4095977fc57cc14e0721cc3b84
keyamitra <keyamitra022@gmail.com>
2021-11-02 01:58:53 +0530

    third comment

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,7 @@ import React from "react";
 class Home extends React.Component{
     render(){
         return(
-            <h1>This is our home page.</h1>
+            <h1>This is our home page it is bad.</h1>
         );
     }
 }
