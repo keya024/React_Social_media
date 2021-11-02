501183c67330839c97bfe12751391fe88bdbf3e9
keyamitra <keyamitra022@gmail.com>
2021-11-02 19:01:53 +0530

    final work

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import Home from './components/Home';
+import Home from './pages/home/Home';
 
 function App() {
   return (
---
+++ src/pages/home/Home.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+class Home extends React.Component{
+    render(){
+        return(
+            <div>
+               <nav>
+                   <div className="nav-left">
+                       <img src="./images/logo.png" className="logo" alt=""></img>
+                   </div>
+               </nav>
+            </div>
+        );
+    }
+}
+
+export default Home;
