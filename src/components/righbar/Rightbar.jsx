3cb11f5d0da281c4e54235335084a472acb82d0d
keyamitra <keyamitra022@gmail.com>
2021-11-04 01:24:20 +0530

    Home updation ui design final

--- src/components/righbar/Rightbar.jsx
+++ src/components/righbar/Rightbar.jsx
@@ -4,7 +4,54 @@ import "./rightbar.css"
 export default function Rightbar(){
     return(
         <div className="rightbar">
-            rightbar
+            <div className="rightbarWrapper">
+                <div className="bithdayContainer">
+                   <img src="images/birthday.jpg" alt="" className="birthdayImg" />
+                   <span className="birthdayTest">
+                       <b>Asmita Paul</b> and 3 other friends have a bithday today.
+                   </span>
+                </div>
+                <img src="images/ad1.jpg" alt="" className="rightbarAd" />
+                <h4 className="rightbarTitle">Online Friends</h4>
+                <hr></hr>
+                <ul className="rightbarFriendList">
+                    <li className="rightbarFriend">
+                        <div className="rightbarProfileImgContainer">
+                            <img src="images/persons/3.jpg" alt="" className="rightbarProfileImg" />
+                            <span className="rightbarOnline"></span>
+                        </div>
+                        <span className="rightbarUsername">Arindom Ghosh</span>
+                    </li>
+                    <li className="rightbarFriend">
+                        <div className="rightbarProfileImgContainer">
+                            <img src="images/persons/3.jpg" alt="" className="rightbarProfileImg" />
+                            <span className="rightbarOnline"></span>
+                        </div>
+                        <span className="rightbarUsername">Arindom Ghosh</span>
+                    </li>
+                    <li className="rightbarFriend">
+                        <div className="rightbarProfileImgContainer">
+                            <img src="images/persons/3.jpg" alt="" className="rightbarProfileImg" />
+                            <span className="rightbarOnline"></span>
+                        </div>
+                        <span className="rightbarUsername">Arindom Ghosh</span>
+                    </li>
+                    <li className="rightbarFriend">
+                        <div className="rightbarProfileImgContainer">
+                            <img src="images/persons/3.jpg" alt="" className="rightbarProfileImg" />
+                            <span className="rightbarOnline"></span>
+                        </div>
+                        <span className="rightbarUsername">Arindom Ghosh</span>
+                    </li>
+                    <li className="rightbarFriend">
+                        <div className="rightbarProfileImgContainer">
+                            <img src="images/persons/3.jpg" alt="" className="rightbarProfileImg" />
+                            <span className="rightbarOnline"></span>
+                        </div>
+                        <span className="rightbarUsername">Arindom Ghosh</span>
+                    </li>
+                </ul>
+            </div>
         </div>
     )
 }
