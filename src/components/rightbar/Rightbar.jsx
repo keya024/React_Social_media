82327a4b9b6e90f08900a93fd49a5b88f5152d7e
keyamitra <keyamitra022@gmail.com>
2021-11-15 21:35:25 +0530

    Profile interface complete

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import "./rightbar.css"
 
-export default function Rightbar({profile}){
+export default function Rightbar(){
     
-    return(
-        <div className="rightbar">
-            <div className="rightbarWrapper">
-                <div className="bithdayContainer">
+    const HomeRightbar = () => {
+        return(
+            <>
+                           <div className="bithdayContainer">
                    <img src="images/birthday.jpg" alt="" className="birthdayImg" />
                    <span className="birthdayTest">
                        <b>Asmita Paul</b> and 3 other friends have a bithday today.
@@ -52,6 +52,63 @@ export default function Rightbar({profile}){
                         <span className="rightbarUsername">Arindom Ghosh</span>
                     </li>
                 </ul>
+            </>
+        )
+    }
+    const ProfileRightbar=()=>{
+        return(
+               <>
+                 <h4 className="rightbarTitle1">User information</h4>
+                 <div className="rightbarInfo">
+                     <div className="rightbarInfoItem">
+                         <span className="rightInfoKey">City:</span>
+                         <span className="rightInfoKeyValue">India</span>
+                     </div>
+                     <div className="rightbarInfoItem">
+                         <span className="rightInfoKey">From:</span>
+                         <span className="rightInfoKeyValue">Kolkata</span>
+                     </div>
+                     <div className="rightbarInfoItem">
+                         <span className="rightInfoKey">Relationship:</span>
+                         <span className="rightInfoKeyValue">Single</span>
+                     </div>
+                 </div>
+                 <h4 className="rightbarTitle1">User Friends</h4>
+                  <div className="rightbarFollowings">
+                      <div className="rightbarFollowing">
+                          <img src="images/persons/8.jpg" alt="" className="rightbarFollowingImg" />
+                          <span className="rightbarFollowingName">Rita Roy</span>
+                      </div>
+                      <div className="rightbarFollowing">
+                          <img src="images/persons/8.jpg" alt="" className="rightbarFollowingImg" />
+                          <span className="rightbarFollowingName">Rita Roy</span>
+                      </div>
+                      <div className="rightbarFollowing">
+                          <img src="images/persons/8.jpg" alt="" className="rightbarFollowingImg" />
+                          <span className="rightbarFollowingName">Rita Roy</span>
+                      </div>
+                      <div className="rightbarFollowing">
+                          <img src="images/persons/8.jpg" alt="" className="rightbarFollowingImg" />
+                          <span className="rightbarFollowingName">Rita Roy</span>
+                      </div>
+                      <div className="rightbarFollowing">
+                          <img src="images/persons/8.jpg" alt="" className="rightbarFollowingImg" />
+                          <span className="rightbarFollowingName">Rita Roy</span>
+                      </div>
+                      <div className="rightbarFollowing">
+                          <img src="images/persons/8.jpg" alt="" className="rightbarFollowingImg" />
+                          <span className="rightbarFollowingName">Rita Roy</span>
+                      </div>
+
+
+                  </div>
+               </>
+            )
+    }
+    return(
+        <div className="rightbar">
+            <div className="rightbarWrapper">
+               <ProfileRightbar />
             </div>
         </div>
     )
