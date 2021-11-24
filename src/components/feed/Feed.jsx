c29051cad8d1e0248f36d3127453ebcbd8d33515
keyamitra <keyamitra022@gmail.com>
2021-11-24 22:32:27 +0530

    before using context Api

--- src/components/feed/Feed.jsx
+++ src/components/feed/Feed.jsx
@@ -4,22 +4,24 @@ import Share from "../share/Share";
 import "./feed.css"
 import axios from "axios"
 
-export default function Feed(){
+export default function Feed({username}){
     const [posts,setPosts] = useState([]);
 
     useEffect(()=>{
       const fetchPosts = async ()=>{
-        const res = await axios.get("posts/timeline/619cf29045343d9059e54d85");
+        const res = username 
+           ? await axios.get("/posts/profile/" + username) 
+           : await axios.get("posts/timeline/619cf29045343d9059e54d85");
         setPosts(res.data)
       }
       fetchPosts();
-    },[]);
+    },[usernames]);
     return(
         <div className="feed">
           <div className="feedWrapper">
             <Share />
             {posts.map((p) => (
-              <Post key={p.id} post={p}/>
+              <Post key={p._id} post={p}/>
             ))}
           </div>
         </div>
